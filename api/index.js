0e9ee6d9ce5129ffa0517242ad1c8378697c3916
Michael Mace <michael.mace@conexon.us>
2024-09-16 13:12:35 -0500

    set up endpoint for board

--- api/index.js
+++ api/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/api', (req, res) => {
+  console.log('👀 🔍 ~ app.get ~ req:', req)
   res.send('test')
+})
+
+app.get('/board', (req, res) => {
+  console.log('👀 🔍 ~ app.get ~ req:', req)
+  res.send('board is here')
 })
 
 module.exports = app
